7bd09be39f1591bf4b9c5b9de29fd68b8f26a1fc
Daniel Cui <cyhonline@pm.me>
2024-10-18 20:19:52 -0600

    Adding google services json and firebase gradle settings

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     alias(libs.plugins.android.application)
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -35,7 +36,7 @@ android {
 }
 
 dependencies {
-
+    implementation(platform("com.google.firebase:firebase-bom:33.4.0"))
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.constraintlayout)
