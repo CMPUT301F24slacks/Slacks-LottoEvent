f62dd3dc014633575a712e53119337f50bd3c1fd
Daniel Cui <cyhonline@pm.me>
2024-10-20 00:49:29 -0600

    Start of EventsHome

--- app/src/main/java/com/example/slacks_lottoevent/EventsHomeActivity.java
+++ app/src/main/java/com/example/slacks_lottoevent/EventsHomeActivity.java
@@ -1,15 +1,46 @@
 package com.example.slacks_lottoevent;
 
 import android.os.Bundle;
-
+import android.view.Menu;
+import android.view.View;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.ui.AppBarConfiguration;
+
+import com.example.slacks_lottoevent.databinding.ActivityEventsHomeBinding;
+import com.google.android.material.appbar.MaterialToolbar;
+import com.google.android.material.snackbar.Snackbar;
 
 public class EventsHomeActivity extends AppCompatActivity {
+
+    private ActivityEventsHomeBinding binding;
+    private AppBarConfiguration appBarConfiguration;
+    private MaterialToolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_events_home);
+
+        binding = ActivityEventsHomeBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        toolbar = binding.toolbar;
+        setSupportActionBar(toolbar);
+
+        binding.qrCodeScannerFAB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAnchorView(R.id.qr_code_scanner_FAB)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_events_home, menu);
+        return true;
     }
 
 
