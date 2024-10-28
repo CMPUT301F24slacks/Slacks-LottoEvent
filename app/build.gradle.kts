a674f911f1d838aca4c6f9a07c44dc5043defde2
leyla qurbanova <gurbanov@ug11.cs.ualberta.ca>
2024-10-28 13:01:19 -0600

    Adding dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -81,9 +81,11 @@ afterEvaluate {
 }
 
 dependencies {
+    implementation("com.google.android.material:material:1.10.0")
     implementation("com.google.guava:guava:31.0.1-android")
     implementation(platform("com.google.firebase:firebase-bom:33.4.0"))
     implementation("com.google.firebase:firebase-firestore")
+    implementation("com.google.firebase:firebase-database")
 
     implementation("androidx.camera:camera-core:1.3.4")
     implementation("androidx.camera:camera-camera2:1.3.4")
@@ -91,4 +93,10 @@ dependencies {
     implementation("androidx.camera:camera-view:1.2.0")
     implementation(libs.zxing.android.embedded.v410)
     implementation(libs.core)
+    implementation(libs.navigation.fragment)
+    implementation(libs.navigation.ui)
+
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    testImplementation("junit:junit:4.13.2")
 }
