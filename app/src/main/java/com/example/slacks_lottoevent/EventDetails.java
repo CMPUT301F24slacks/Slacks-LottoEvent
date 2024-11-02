d137a316bf74d106a66899ad159a4b3f31d9c427
Jonathan Scruggs <30415863+Jonathan-Scruggs@users.noreply.github.com>
2024-11-01 22:08:49 -0600

    Added the functionality for the event details page to show the event details for that specific qr code

--- app/src/main/java/com/example/slacks_lottoevent/EventDetails.java
+++ app/src/main/java/com/example/slacks_lottoevent/EventDetails.java
@@ -1,18 +1,24 @@
 package com.example.slacks_lottoevent;
 
-import android.content.DialogInterface;
+
 import android.os.Bundle;
+
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import com.google.firebase.firestore.DocumentReference;
+
+import com.example.slacks_lottoevent.databinding.ActivityEventDetailsBinding;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
+import java.util.Map;
+
 public class EventDetails extends AppCompatActivity {
+    private ActivityEventDetailsBinding binding;
+
 
     private TextView eventTitle, eventDate, eventLocation, eventDescription, eventCapacity;
     private Button joinButton;
@@ -20,15 +26,44 @@ public class EventDetails extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_event_details);
+        binding = ActivityEventDetailsBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
         String qrCodeValue = getIntent().getStringExtra("qrCodeValue");
-        System.out.println(qrCodeValue);
+
         FirebaseFirestore db = FirebaseFirestore.getInstance();
-        db.collection("events").whereEqualTo("eventID", qrCodeValue).get()
+        db.collection("events").whereEqualTo("eventDetails.eventID", qrCodeValue).get()
                 .addOnCompleteListener(task -> {
                     if (task.isSuccessful() && !task.getResult().isEmpty()) {
+
                         DocumentSnapshot document = task.getResult().getDocuments().get(0);
                         System.out.println(document);
+                        Map<String, Object> eventDetails = (Map<String, Object>) document.get("eventDetails");
+
+
+
+                        String eventName = (String) eventDetails.get("name");
+                        String date = (String) eventDetails.get("date");
+                        String description = (String) eventDetails.get("description");
+                        Long capacity = (Long) eventDetails.get("capacity");
+                        Long pplSelected = document.getLong("pplSelected");
+                        String time = document.getString("time");
+                        System.out.println("eventName"+eventName);
+                        binding.eventTitle.setText(eventName);
+                        binding.eventDate.setText(date);
+                        assert capacity != null;
+                        String capacityAsString = capacity.toString();
+                        Map<String, Object> facilityDetails =  (Map<String, Object>) eventDetails.get("facility");
+                        String location = (String) facilityDetails.get("streetAddress1") + ", "+ facilityDetails.get("city") + ", " + facilityDetails.get("country") + ", " + facilityDetails.get("postalCode");
+                        binding.eventLocation.setText(location);
+                        String waitlistCapcity = "Waitlist Capacity" + capacityAsString;
+                        binding.eventWaitlistCapacity.setText(waitlistCapcity);
+                        binding.eventDescription.setText(description);
+
+
+
+
+
+
                     }
                 });
 
