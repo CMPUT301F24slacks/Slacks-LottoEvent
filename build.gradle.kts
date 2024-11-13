c7280d70cc6a7e8955d3a3854e6af2556e648ba6
Jonathan Scruggs <30415863+Jonathan-Scruggs@users.noreply.github.com>
2024-11-13 16:32:16 -0700

    local.default.properties

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@
 plugins {
     alias(libs.plugins.android.application) apply false
     id("com.google.gms.google-services") version "4.4.2" apply false
-
+    id("com.google.android.libraries.mapsplatform.secrets-gradle-plugin") version "2.0.0"
 }
 buildscript {
     dependencies {
